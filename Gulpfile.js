3512b9e1415f1fae4a52d2bf28df7197bf5a4e57
IAMATinyCoder <christopher.kenrick@gmail.com>
2015-06-21 17:29:58 -0400

    File Updates

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,32 @@
-var gulp = require('gulp'),
+'use strict';
+
+var watchify = require('watchify'),
+    browserify = require('browserify'),
+    gulp = require('gulp'),
     bourbon = require('node-bourbon').includePaths,
     sass = require('gulp-sass'),
-    neat = require('node-neat').includePaths;
+    neat = require('node-neat').includePaths,
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    gutil = require('gulp-util'),
+    sourcemaps = require('gulp-sourcemaps'),
+    assign = require('lodash.assign');
 
 var paths = {
     scss: './sass/*.scss'
 };
+// add custom browserify options here
+var customOpts = {
+  entries: ['./src/index.js'],
+  debug: true
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
 
+// add transformations here
+// i.e. b.transform(coffeeify);
+
+gulp.task('js', bundle); // so you can run `gulp js` to build the file
 gulp.task('sass', function () {
     return gulp.src(paths.scss)
         .pipe(sass({
@@ -15,7 +35,23 @@ gulp.task('sass', function () {
         }))
         .pipe(gulp.dest('./dist/css'));
 });
-
 gulp.task('default',function(){
     gulp.start('styles');
 });
+
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
+
+function bundle() {
+  return b.bundle()
+    // log errors if they happen
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('bundle.js'))
+    // optional, remove if you don't need to buffer file contents
+    .pipe(buffer())
+    // optional, remove if you dont want sourcemaps
+    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+       // Add transformation tasks to the pipeline here.
+    .pipe(sourcemaps.write('./')) // writes .map file
+    .pipe(gulp.dest('./dist'));
+}
